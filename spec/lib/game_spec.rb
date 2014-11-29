17b9d05015050daaee7e11a007ba71491645bd2e
manitp <manitpadungkit@gmail.com>
2014-11-29 18:32:20 +0700

    5

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -110,7 +110,7 @@ describe Game do
 			g4.roll(2)
 			g4.roll(3)
 			#no row3
-			expect(g4.score).to eq(112)
+			expect(g4.score).to eq(111)
 		end
 		it 'can roll a simple game (no spare or strike)' do
 	    	g5 = Game.new
